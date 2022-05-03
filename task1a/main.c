29741ee631d1c8ebd62d077569f5e165ed9fd17e
daniel-ubuntu <elhadan@post.bgu.ac.il>
2022-05-04 00:51:36 +0300

    refactored err print, removed it

--- task1a/main.c
+++ task1a/main.c
@@ -13,27 +13,6 @@ extern int system_call();
 #define STDERR 2
 
 
-
-int ClearSpaces1 (char * c, int len, int from) {
-    int len1 = len;
-    while (len1 != 0) {
-        if(*c == ' ') {
-            while (*c == ' ')
-                system_call(SYS_READ,from, c, 1);
-            return len1;
-        }
-        else if( *c != ' ' && *c != '\n') {
-            len1 = system_call(SYS_READ,from, c, 1);
-            if (len1 == 0)
-                return len1;
-        }
-        else
-            return len1;
-    }
-    return len1;
-
-}
-
 int print_to_err(int call_id, int response, int flag) {
     if (flag == 'D') {
         system_call(SYS_WRITE, STDERR, strcat(itoa(call_id), "\n"), 3);
@@ -42,20 +21,22 @@ int print_to_err(int call_id, int response, int flag) {
     return 0;
 }
 
-int ClearSpaces1err (char * c, int len, int from) {
+
+int ClearSpaces1 (char * c, int len, int from, int flag) {
     int len1 = len;
     while (len1 != 0) {
         if(*c == ' ') {
             while (*c == ' ') {
                 len1 = system_call(SYS_READ,from, c, 1);
-                print_to_err(SYS_READ,len1);
+                print_to_err(SYS_READ,len1, flag);
+
             }
 
             return len1;
         }
         else if( *c != ' ' && *c != '\n') {
             len1 = system_call(SYS_READ,from, c, 1);
-            print_to_err(SYS_READ,len1);
+            print_to_err(SYS_READ,len1, flag);
             if (len1 == 0)
                 return len1;
         }
@@ -64,47 +45,6 @@ int ClearSpaces1err (char * c, int len, int from) {
     }
     return len1;
 
-}
-
-
-
-
-int D_flag(int from,int to) {
-    char* c = itoa(222);
-    int res = 0;
-    int len = system_call(SYS_READ,from, c, 1);
-    int write = 0;
-    print_to_err(SYS_READ,len);
-    while (len != 0) {
-        if(*c != ' ' && *c != '\n') {
-            res++;
-            len = ClearSpaces1err(c, len, from);
-        }
-        else if( *c == '\n') {
-            write = system_call(SYS_WRITE,to, strcat(itoa(res),"\n"), 3);
-            print_to_err(SYS_WRITE,write);
-            res = 0;
-            len = system_call(SYS_READ,from, c, 1);
-            print_to_err(SYS_READ,len);
-        }
-        if(len == 0 && *c != '\n') {
-            write = system_call(SYS_WRITE,to, strcat(itoa(res),"\n"), 2);
-            print_to_err(SYS_WRITE,write);
-        }
-        if(from != STDIN) {
-            int r = system_call(SYS_CLOSE,from, c, 1);
-            print_to_err(SYS_CLOSE,r);
-        }
-
-        if(to != STDOUT) {
-            int t = system_call(SYS_CLOSE,to, c, 1);
-            print_to_err(SYS_CLOSE,t);
-        }
-
-
-    }
-
-    return 0;
 }
 
 
@@ -131,47 +71,53 @@ int main (int argc , char* argv[], char* envp[]) {
             file_from = system_call(SYS_OPEN,str, 000, 0644);
             if(file_from < 0)
                 system_call(1);
-            if(flag == 'D')
-                print_to_err(SYS_OPEN,file_from);
+            print_to_err(SYS_OPEN,file_from, flag);
 
         }
         else if(argv[k][1] == 'o'){
             file_to = system_call(SYS_OPEN,str, 101, 0644);
             if(file_to < 0)
                 system_call(1);
-            if(flag == 'D')
-                print_to_err(SYS_OPEN,file_to);
+            print_to_err(SYS_OPEN,file_to,flag);
         }
     }
 
-    if (flag == 'D') {
-        D_flag(file_from,file_to);
-    }
-    else {
         char* c = itoa(222);
         int res = 0;
+        int response;
         int len = system_call(SYS_READ,file_from, c, 1);
+        print_to_err(SYS_READ,len,flag);
         while (len != 0) {
             if(*c != ' ' && *c != '\n') {
                 res++;
-                len = ClearSpaces1(c, len, file_from);
+                len = ClearSpaces1(c, len, file_from, flag);
 
             }
             else if( *c == '\n') {
-                system_call(SYS_WRITE,file_to, strcat(itoa(res),"\n"), 2);
+                response = system_call(SYS_WRITE,file_to, strcat(itoa(res),"\n"), 2);
+                print_to_err(SYS_WRITE,response,flag);
                 res = 0;
                 len = system_call(SYS_READ,file_from, c, 1);
+                print_to_err(SYS_READ,len,flag);
+
             }
             if(len == 0 && *c != '\n') {
-                system_call(SYS_WRITE,file_to, strcat(itoa(res),"\n"), 2);
+                response = system_call(SYS_WRITE,file_to, strcat(itoa(res),"\n"), 2);
+                print_to_err(SYS_WRITE,response,flag);
+
             }
 
         }
-        if(file_from != STDIN)
-            system_call(SYS_CLOSE,file_from, c, 1);
-        if(file_to != STDOUT)
-            system_call(SYS_CLOSE,file_to, c, 1);
-    }
+        if(file_from != STDIN) {
+            response = system_call(SYS_CLOSE,file_from, c, 1);
+            print_to_err(SYS_CLOSE,response,flag);
+
+        }
+        if(file_to != STDOUT) {
+            response = system_call(SYS_CLOSE,file_to, c, 1);
+            print_to_err(SYS_CLOSE,response,flag);
+        }
+
 
     return 0;
 }
